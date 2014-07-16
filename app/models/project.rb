d437d501ce2a7f274b87f44f91d781e1f5f9f519
Richard Martin <richardgeorgemartin@gmail.com>
2014-07-16 17:49:24 -0400

    Adding User associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :fundings
   has_many :rewards
+  belongs_to :hacker, class_name: "User"
+  has_many :backers, through: :fundings, source: :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716191428) do
+ActiveRecord::Schema.define(version: 20140716202628) do
 
   create_table "fundings", force: true do |t|
     t.integer  "amount"
@@ -41,5 +41,19 @@ ActiveRecord::Schema.define(version: 20140716191428) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                        null: false
+    t.string   "crypted_password",             null: false
+    t.string   "salt",                         null: false
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
 
 end
